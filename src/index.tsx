03337ef8e3bca99d26958418f125ff108742f283
Kevin <kevin@kegisiteweb.com>
2019-10-29 13:20:44 -0400

    disabled redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,19 @@ import './../assets/scss'
 
 import React from 'react'
 import ReactDom from 'react-dom'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import store from './store'
-import App from './App'
+// import { Provider } from 'react-redux'
+// import store from './store'
+import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDom.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
-    </Provider>,
-    document.getElementById('app')
+    <App/>,
+    document.getElementById('app'),
   )
 })
+
+/*
+<Provider store={store}>
+  <App/>
+</Provider>
+*/
